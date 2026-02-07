52343c08c809dd9ddfdf22b0d3f62e1bfe51dcf7
N.DERAJ <nchibachidera0@gmail.com>
2026-02-07 20:44:50 +0100

    updated project with latest changes

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -18,6 +18,7 @@ const LoginPage = () => {
       [name]: type === 'checkbox' ? checked : value,
     }))
   }
+  h
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     await login(formData.email, formData.password)
